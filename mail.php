88869435c5ec6ac3c6b888a6f4ab2cd2c4c50540
Jo Mendes <jlmendes1996@gmail.com>
2021-04-07 21:16:15 -0400

    mail handler

---
+++ mail.php
@@ -0,0 +1,8 @@
+$subject = "You have a new inquiry!"
+
+$name = POST[name]/n/n;
+$email = POST[email]/n/n;
+$message = POST[message]/n/n;
+
+
+mail(Daniithepa@gmail.com, $subject,$name, $email, $message);
